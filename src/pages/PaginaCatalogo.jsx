739641bc3da9b4de65ad8b4cbb04f808bb147665
Christian Cámara <chcamaraguillen@gmail.com>
2025-11-22 12:25:35 +0100

    cración de la página del catálogo completo de camisetas

---
+++ src/pages/PaginaCatalogo.jsx
@@ -0,0 +1,35 @@
+import Catalogo from "../components/Catalogo";
+import Camiseta from "../components/Camiseta";
+import camisetas from "../data/camisetas";
+
+function PaginaCatalogo() {
+  return (
+
+    <Catalogo>
+      <h1 className="font-heading-h1 leading-(--heading-h1-line-height) text-(--colorprimary) [text-shadow:0px_4px_4px_#00000040]">
+        Camisetas de Fútbol
+      </h1>
+      <p className="body-text">
+        Nuestro Catálogo de camisetas de fútbol:
+      </p>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full mt-8">
+
+        {camisetas.map((camiseta, index) => (
+          <Camiseta
+            key={camiseta.id ?? index}
+            id={camiseta.id} 
+            nombre={camiseta.nombre}
+            descripcion={camiseta.descripcion}
+            precio={camiseta.precio}
+            categoria={camiseta.categoria}
+            foto={camiseta.imagen}
+          />
+        ))}
+
+      </div>
+    </Catalogo>
+  )
+}
+
+export default PaginaCatalogo
