0e0918b667d444eac43208f89b170ef6fcd50370
Christian Cámara <chcamaraguillen@gmail.com>
2025-12-28 17:47:21 +0100

    modificación de App para dejar simplemente Header, Router y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import CamisetaDetalle from "./pages/CamisetaDetalle";
-import ErrorPage from "./pages/ErrorPage";
-import PaginaCatalogo from "./pages/PaginaCatalogo";
+import Router from "./components/Router";
 
 function App() {
   return (
     <>
       <Header />
-
-      <Routes>
-
-        {/* Página principal */}
-        <Route path="/" element={<Home />} />
-
-        {/* Página de Inicio */}
-        <Route path="/inicio" element={<Navigate to="/" />} />
-
-        {/* Página de detalles de la camiseta */}
-        <Route path="/camiseta/:id" element={<CamisetaDetalle />} />
-
-        {/* Página de catálogo completo de las camisetas */}
-        <Route path="/paginacatalogo" element={<PaginaCatalogo></PaginaCatalogo>}></Route>
-
-        {/* Página de error */}
-        <Route path="*" element={<ErrorPage />} />
-
-      </Routes>
-
+      <Router/>
       <Footer />
     </>
   );
