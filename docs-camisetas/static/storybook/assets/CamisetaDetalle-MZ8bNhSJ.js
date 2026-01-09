3500f2a83eabe11538470c2f69ea780a6331f98e
Christian Cámara <chcamaraguillen@gmail.com>
2026-01-09 12:43:41 +0100

    todo el contenido instalado de StoryBook

---
+++ docs-camisetas/static/storybook/assets/CamisetaDetalle-MZ8bNhSJ.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as s}from"./index-Y1DwEpNG.js";import{M as t,P as l,C as o,S as a}from"./blocks-B_EAMI0K.js";import{C as c,B as d,R as m}from"./CamisetaDetalle.stories-B2qbrID2.js";import"./iframe-DPzJoFxg.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D7k7TZ__.js";function r(i){const n={code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(t,{of:c}),`
+`,e.jsxs(n.h1,{id:"detalle-de-camiseta-camisetadetalle-",children:["Detalle de Camiseta (",e.jsx(n.code,{children:"<CamisetaDetalle />"}),")"]}),`
+`,e.jsx(n.h2,{id:"descripción",children:"Descripción"}),`
+`,e.jsxs(n.p,{children:[`Este componente muestra la información completa de una camiseta de fútbol.\r
+Obtiene el producto a partir del parámetro `,e.jsx(n.code,{children:"id"})," de la URL y muestra:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Imagen del producto"}),`
+`,e.jsx(n.li,{children:"Nombre"}),`
+`,e.jsx(n.li,{children:"Descripción"}),`
+`,e.jsx(n.li,{children:"Precio"}),`
+`,e.jsx(n.li,{children:"Categoría"}),`
+`,e.jsx(n.li,{children:"Botón para volver atrás"}),`
+`]}),`
+`,e.jsx(n.h2,{id:"vista-principal",children:"Vista principal"}),`
+`,e.jsx(l,{}),`
+`,e.jsx(o,{}),`
+`,e.jsx(n.h2,{id:"ejemplos",children:"Ejemplos"}),`
+`,e.jsx(n.h3,{id:"camiseta-fc-barcelona",children:"Camiseta FC Barcelona"}),`
+`,e.jsx(a,{of:d}),`
+`,e.jsx(n.h3,{id:"camiseta-real-madrid",children:"Camiseta Real Madrid"}),`
+`,e.jsx(a,{of:m})]})}function D(i={}){const{wrapper:n}={...s(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(r,{...i})}):r(i)}export{D as default};
