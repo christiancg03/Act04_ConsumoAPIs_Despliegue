71834a5831f1550a6d83e2c4483d6a092bb42e60
Christian Cámara <chcamaraguillen@gmail.com>
2026-01-10 18:31:59 +0100

    texto cambiado en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="w-full bg-green-600 text-white py-4">
       <div className="max-w-7xl mx-auto text-center px-6">
-        <p className="text-sm">© {new Date().getFullYear()} Tienda de Camisetas. Todos los derechos reservados.</p>
+        <p className="text-sm">© {new Date().getFullYear()} Futbol Maniac. Todos los derechos reservados.</p>
         <p className="text-sm mt-1">Síguenos en redes: 
           <span className="ml-2 underline cursor-pointer">Facebook</span>, 
           <span className="ml-2 underline cursor-pointer">Instagram</span>, 
