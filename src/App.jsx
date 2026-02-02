439a8c70189e74bc9549279040be5ee60350462d
Christian Cámara <chcamaraguillen@gmail.com>
2026-02-02 16:01:08 +0100

    Añadido del UserProvider en el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Router from "./components/Router";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
     <>
-      <Header />
-      <Router/>
-      <Footer />
+      <UserProvider>
+        <Header />
+        <Router />
+        <Footer />
+      </UserProvider>
     </>
   );
 }
