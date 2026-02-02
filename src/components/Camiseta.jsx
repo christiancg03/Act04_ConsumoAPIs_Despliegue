167179c6d2619cce5e5261dcd55d13b90dabf706
Christian Cámara <chcamaraguillen@gmail.com>
2026-02-02 16:10:01 +0100

    usamos el userLogged del UserContext para todo el tema del borrado de camisetas

--- src/components/Camiseta.jsx
+++ src/components/Camiseta.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 /**
  * Componente que representa una tarjeta de producto de camiseta.
@@ -28,15 +30,16 @@ import { Link } from "react-router-dom";
  * />
  */
 function Camiseta(props) {
-  const { id, foto, nombre, descripcion, precio, categoria } = props;
+  const { id, foto, nombre, descripcion, precio, categoria, onDelete } = props;
+  const { userLogged } = useContext(UserContext);
 
   return (
     <Link to={`/camiseta/${id}`} className="w-full">
-    <article
-      tabIndex="0"
-      aria-label={`Camiseta ${nombre}`}
-      className="flex flex-col items-center gap-3 p-4 rounded-lg bg-white shadow-md hover:shadow-lg transition text-center"
-    >
+      <article
+        tabIndex="0"
+        aria-label={`Camiseta ${nombre}`}
+        className="flex flex-col items-center gap-3 p-4 rounded-lg bg-white shadow-md hover:shadow-lg transition text-center"
+      >
         <figure className="w-full aspect-square rounded-lg bg-gray-100 overflow-hidden">
           <img
             src={foto}
@@ -46,20 +49,35 @@ function Camiseta(props) {
           />
         </figure>
 
-      {/* Nombre */}
-      <header>
-        <h2 className="font-heading-h5">{nombre}</h2>
-      </header>
+        {/* Nombre */}
+        <header>
+          <h2 className="font-heading-h5">{nombre}</h2>
+        </header>
 
-      {/* Descripción */}
-      <p className="body-text text-sm">{descripcion}</p>
+        {/* Descripción */}
+        <p className="body-text text-sm">{descripcion}</p>
 
-      {/* Precio */}
-      <p className="body-text-bold text-sm">Precio: {precio}€</p>
+        {/* Precio */}
+        <p className="body-text-bold text-sm">Precio: {precio}€</p>
 
-      {/* Categoría */}
-      <p className="body-text text-sm">{categoria}</p>
-    </article>
+        {/* Categoría */}
+        <p className="body-text text-sm">{categoria}</p>
+
+        {userLogged && (
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            onDelete(id);
+          }}
+          className="mt-2 px-3 py-1 bg-red-600 text-white rounded-lg hover:bg-red-700"
+        >
+          Eliminar
+        </button>
+        )}
+
+      </article>
+
     </Link>
   );
 }
