9f9ef72d3217c07456e982cfa680ca835bc80ad5
Christian Cámara <chcamaraguillen@gmail.com>
2026-02-02 16:02:28 +0100

    creación del componente para rutas protegidas, sustitución de la antigua ruta del Admin por la nueva ruta y añadido de la ruta de login

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -5,6 +5,17 @@ import CamisetaDetalle from "./CamisetaDetalle";
 import PaginaCatalogo from "../pages/PaginaCatalogo";
 import Admin from "../pages/Admin";
 import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+// Componente para rutas protegidas
+const PrivateRoute = ({ children }) => {
+    const { userLogged } = useContext(UserContext);
+    if (!userLogged) return <Navigate to="/login" replace />;
+    return children;
+};
+
 
 function Router() {
     return (
@@ -14,7 +25,8 @@ function Router() {
                 <Route path="/inicio" element={<Navigate to="/" />} />
                 <Route path="/camiseta/:id" element={<CamisetaDetalle />} />
                 <Route path="/paginacatalogo" element={<PaginaCatalogo></PaginaCatalogo>}></Route>
-                <Route path="/admin" element={<Admin />}></Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>}></Route>
             </Route>
 
             {/* Ruta 404 */}
